c9025443925459d330deffc397c5d8cdb11a46e4
Oluwatobiloba Nathaniel Ogunye <ogunye4nao@gmail.com>
2021-03-19 12:56:36 +0100

    Refactored LibPhoneNumberPlugin to use Platform Interface instead of Method Channel

--- libphonenumber_web/lib/src/libphonenumber_plugin.dart
+++ libphonenumber_web/lib/src/libphonenumber_plugin.dart
@@ -1,52 +1,11 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:libphonenumber_platform_interface/libphonenumber_platform_interface.dart';
 import 'package:libphonenumber_web/src/interop/libphonenumber_interop.dart';
 
 class LibPhoneNumberPlugin extends LibPhoneNumberPlatform {
   static void registerWith(Registrar registrar) {
-    final MethodChannel methodChannel = MethodChannel(
-      'plugin.libphonenumber',
-      const StandardMethodCodec(),
-      registrar.messenger,
-    );
     final LibPhoneNumberPlugin instance = LibPhoneNumberPlugin();
     LibPhoneNumberPlatform.instance = instance;
-    methodChannel.setMethodCallHandler(instance.handleMethodCall);
-  }
-
-  Future<dynamic> handleMethodCall(MethodCall call) async {
-    switch (call.method) {
-      case 'formatAsYouType':
-        String phoneNumber = call.arguments['phoneNumber'];
-        String isoCode = call.arguments['isoCode'];
-        return formatAsYouType(phoneNumber, isoCode);
-      case 'getNameForNumber':
-        String phoneNumber = call.arguments['phoneNumber'];
-        String isoCode = call.arguments['isoCode'];
-        return getNameForNumber(phoneNumber, isoCode);
-      case 'getNumberType':
-        String phoneNumber = call.arguments['phoneNumber'];
-        String isoCode = call.arguments['isoCode'];
-        return getNumberType(phoneNumber, isoCode);
-      case 'getRegionInfo':
-        String phoneNumber = call.arguments['phoneNumber'];
-        String isoCode = call.arguments['isoCode'];
-        return getRegionInfo(phoneNumber, isoCode);
-      case 'isValidNumber':
-        String phoneNumber = call.arguments['phoneNumber'];
-        String isoCode = call.arguments['isoCode'];
-        return isValidNumber(phoneNumber, isoCode);
-      case 'normalizePhoneNumber':
-        String phoneNumber = call.arguments['phoneNumber'];
-        String isoCode = call.arguments['isoCode'];
-        return normalizePhoneNumber(phoneNumber, isoCode);
-      default:
-        throw PlatformException(
-            code: 'Unimplemented',
-            details: "The libphonenumber plugin for web doesn't implement "
-                "the method '${call.method}'");
-    }
   }
 
   @override
